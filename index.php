b0550b0f7c312a345febbfa5b10c0c7ef6a0aa57
Md Shajib Azher <32217069+mdshajibazher@users.noreply.github.com>
2022-10-10 21:36:40 +0600

    Cors error fix

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        // may also be using PUT, PATCH, HEAD etc
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+\Stripe\Stripe::setApiKey('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+function calculateOrderAmount(array $items): int {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+}
+
+header('Content-Type: application/json');
+
+try {
+    // retrieve JSON from POST body
+    $jsonStr = file_get_contents('php://input');
+    $jsonObj = json_decode($jsonStr);
+
+    // Create a PaymentIntent with amount and currency
+    $paymentIntent = \Stripe\PaymentIntent::create([
+        'amount' => calculateOrderAmount($jsonObj->items),
+        'currency' => 'usd',
+        'automatic_payment_methods' => [
+            'enabled' => true,
+        ],
+    ]);
+
+    $output = [
+        'clientSecret' => $paymentIntent->client_secret,
+    ];
+
+    echo json_encode($output);
+} catch (Error $e) {
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
